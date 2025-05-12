d5fb2858a1e2447f1260dad8cd8d7b0c0934566b
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2025-05-12 15:13:22 +0000

    update

--- setup.py
+++ setup.py
@@ -18,4 +18,22 @@ setup(
     author="Bhimraj Yadav",
     description="Visualize weekly GitHub commit activity across repositories.",
     license="MIT",
+    url="https://github.com/bhimrazy/gh-weekly-commits",
+    project_urls={
+        "Bug Tracker": "https://github.com/bhimrazy/gh-weekly-commits/issues",
+        "Documentation": "https://github.com/bhimrazy/gh-weekly-commits#readme",
+        "Source Code": "https://github.com/bhimrazy/gh-weekly-commits",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
 )
